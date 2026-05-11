6026b308297b12e98ccfe29fdd56f1fe4ba69011
Meilyn F <92810092+meylin103@users.noreply.github.com>
2026-05-11 23:18:28 +0000

    Implement functional user registration endpoint

--- src/api/routes.py
+++ src/api/routes.py
@@ -6,6 +6,13 @@ from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
@@ -21,9 +28,10 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+# //////////////////////////////////////////////////////////registro 
 
 
-@app.route('/user', methods=['POST'])
+@api.route('/user', methods=['POST'])
 def create_user():
     # siempre en formato JSON
     data= request.get_json()
@@ -34,19 +42,23 @@ def create_user():
 
     #extraer los valores de los campos 
     email= data.get("email")
-    password= data.get("password")
-    is_active= data.get("is_active", False)
+
+    username= data.get("username")
 
     #validar si el email esta registrado
     existing_user = User.query.filter_by(email=email).first()
     if existing_user:
         return jsonify({"msg": "ya existe un usuario registrado con ese email"}), 409
     
+#esta linea genera la clave encriptada
+    hashed_password = generate_password_hash(data["password"])
+
+    
     #crea un registro nuevo 
     new_user= User(
         email= email,
-        password= password,
-        is_active= is_active
+        password=  hashed_password,
+        username= username
     )
 
     #debe ser guardada en la base de datos
@@ -61,3 +73,38 @@ def create_user():
          #en caso de error se captura la excepcion
         print(f"Error al crear usuario: {error}")
         return jsonify({"msg": "Internal Server Error", "error": str(error)}), 500
+    
+
+    # //////////////////////////////////////////////////////// login 
+
+
+@api.route("/login", methods=["POST"])
+def login():
+    # username = request.json.get("username", None)
+    # password = request.json.get("password", None)
+
+    # if username != "test" or password != "test":
+    #     return jsonify({"msg": "Bad username or password"}), 401
+
+
+    data= request.get_json()
+
+    user= User.query.filter_by(email=data["email"]).first()
+    if not user or not check_password_hash(user.password, data["password"]):
+        return jsonify({"msg": "Invalid email or password"}), 401
+
+
+    access_token = create_access_token(identity=str(user.id))
+    return jsonify({
+        "token": access_token,
+        "user": user.serialize()}), 200
+
+# ////////////////////////////////////////////////////////////// ruta protegida
+
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+
